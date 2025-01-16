97d4e92ba5591e9886adf59b63e8c4ef814aa11f
Hugues Bruant <hugues.bruant@gmail.com>
2025-01-16 00:33:56 -0800

    repos: add flask

---
+++ repos/flask/hook.py
@@ -0,0 +1,20 @@
+from typing import AbstractSet, Mapping
+
+from prunepytest.api import DefaultHook
+
+class FlaskPruneHook(DefaultHook):
+    def dynamic_dependencies(self) -> Mapping[str, AbstractSet[str]]:
+        return {
+            # importlib entrypoint resolution...
+            "tests/test_cli.py": {"cliapp.app", "cliapp.factory"}
+        }
+
+    #
+    # def filter_irrelevant_files(self, files: AbstractSet[str]) -> AbstractSet[str]:
+    #     return {
+    #         f for f in files
+    #         if not (
+    #             f.startswith("docs/")
+    #             or f.endswith(".md")
+    #         )
+    #     }
